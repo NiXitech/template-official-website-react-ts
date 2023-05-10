37fcb7a36a20fc45376893da1fb936bc8c376381
weifanyang <1123642601@qq.com>
2023-05-10 22:09:19 +0800

    feat: H5 & PC适配

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-// import 'lib-flexible';
 import './i18';
+// import 'lib-flexible';
 import 'amfe-flexible';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
