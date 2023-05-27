54c9698474716615ebd2940c81101cf2e6258ca1
weifanyang <1123642601@qq.com>
2023-05-27 17:03:53 +0800

    feat: 配置全局less

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const VARIABLE_LESS_PATH = './src/variable.less';
+
 const env = process.env.NODE_ENV;
 
 let apiBaseUrl = 'https://api.example.com';
@@ -21,6 +23,7 @@ export default defineConfig({
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
+        additionalData: `@import "${VARIABLE_LESS_PATH}";`,
       },
     },
   },
